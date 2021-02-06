4cc0ce9a131f2f403dfcd974388b14a09264efb9
anishhemachandran <anishh2003@yahoo.com>
2021-02-06 13:44:36 +0000

    Added back navigation to page

--- lib/subreddit_posts_page.dart
+++ lib/subreddit_posts_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fetch_reddit_posts.dart';
 
 class SubRedditPostPage extends StatefulWidget {
   final List subRedditData;
@@ -11,71 +12,86 @@ class SubRedditPostPage extends StatefulWidget {
 class _SubRedditPostPageState extends State<SubRedditPostPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Reddit Sub Post'),
-        ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 20.0,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text('Reddit Sub Post'),
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_left,
+                size: 40,
+              ),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FetchRedditPosts(),
+                ),
+              ),
             ),
-            Expanded(
-                child: ListView(
-              children: [
-                Text(widget.subRedditData[0]['data']['children'][0]['data']
-                    ['title']),
-                Text(widget.subRedditData[0]['data']['children'][0]['data']
-                    ['selftext']),
-                SizedBox(
-                  height: 20.0,
-                  child: Divider(
-                    //height: 10.0,
-                    thickness: 2.0,
-                    color: Colors.black54,
+          ),
+          body: Column(
+            children: [
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                  child: ListView(
+                children: [
+                  Text(widget.subRedditData[0]['data']['children'][0]['data']
+                      ['title']),
+                  Text(widget.subRedditData[0]['data']['children'][0]['data']
+                      ['selftext']),
+                  SizedBox(
+                    height: 20.0,
+                    child: Divider(
+                      //height: 10.0,
+                      thickness: 2.0,
+                      color: Colors.black54,
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Text(
-                    'Comments ',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                  Expanded(
+                    child: Text(
+                      'Comments ',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                  child: Divider(
-                    //height: 10.0,
-                    thickness: 2.0,
-                    color: Colors.black54,
+                  SizedBox(
+                    height: 20.0,
+                    child: Divider(
+                      //height: 10.0,
+                      thickness: 2.0,
+                      color: Colors.black54,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 300.0,
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return Column(
-                        children: [
-                          Card(
-                            child: ListTile(
-                              title: Text(
-                                  'Author: ${widget.subRedditData[1]['data']['children'][index]['data']['author']}'),
-                              subtitle: Text(widget.subRedditData[1]['data']
-                                  ['children'][index]['data']['body']),
-                            ),
-                            color: Colors.white70,
-                          )
-                        ],
-                      );
-                    },
-                    itemCount:
-                        widget.subRedditData[1]['data']['children'].length,
+                  SizedBox(
+                    height: 300.0,
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Card(
+                              child: ListTile(
+                                title: Text(
+                                    'Author: ${widget.subRedditData[1]['data']['children'][index]['data']['author']}'),
+                                subtitle: Text(widget.subRedditData[1]['data']
+                                    ['children'][index]['data']['body']),
+                              ),
+                              color: Colors.white70,
+                            )
+                          ],
+                        );
+                      },
+                      itemCount:
+                          widget.subRedditData[1]['data']['children'].length,
+                    ),
                   ),
-                ),
-              ],
-            ))
-          ],
-        ));
+                ],
+              ))
+            ],
+          )),
+    );
   }
 }
