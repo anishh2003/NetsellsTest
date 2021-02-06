7c9222527e0a2c5cbb118fc5b69cbcddd9783a99
anishhemachandran <anishh2003@yahoo.com>
2021-02-06 14:12:21 +0000

    edited UI

--- lib/subreddit_posts_page.dart
+++ lib/subreddit_posts_page.dart
@@ -31,66 +31,70 @@ class _SubRedditPostPageState extends State<SubRedditPostPage> {
               ),
             ),
           ),
-          body: Column(
-            children: [
-              SizedBox(
-                height: 20.0,
-              ),
-              Expanded(
-                  child: ListView(
-                children: [
-                  Text(widget.subRedditData[0]['data']['children'][0]['data']
-                      ['title']),
-                  Text(widget.subRedditData[0]['data']['children'][0]['data']
-                      ['selftext']),
-                  SizedBox(
-                    height: 20.0,
-                    child: Divider(
-                      //height: 10.0,
-                      thickness: 2.0,
-                      color: Colors.black54,
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(
+                    child: ListView(
+                  children: [
+                    Text(widget.subRedditData[0]['data']['children'][0]['data']
+                        ['title']),
+                    Text(widget.subRedditData[0]['data']['children'][0]['data']
+                        ['selftext']),
+                    SizedBox(
+                      height: 20.0,
+                      child: Divider(
+                        //height: 10.0,
+                        thickness: 2.0,
+                        color: Colors.black54,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      'Comments ',
-                      style: TextStyle(
-                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    SizedBox(
+                      height: 20.0,
+                      child: Text(
+                        'Comments ',
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                    child: Divider(
-                      //height: 10.0,
-                      thickness: 2.0,
-                      color: Colors.black54,
+                    SizedBox(
+                      height: 20.0,
+                      child: Divider(
+                        //height: 10.0,
+                        thickness: 2.0,
+                        color: Colors.black54,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 300.0,
-                    child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        return Column(
-                          children: [
-                            Card(
-                              child: ListTile(
-                                title: Text(
-                                    'Author: ${widget.subRedditData[1]['data']['children'][index]['data']['author']}'),
-                                subtitle: Text(widget.subRedditData[1]['data']
-                                    ['children'][index]['data']['body']),
-                              ),
-                              color: Colors.white70,
-                            )
-                          ],
-                        );
-                      },
-                      itemCount:
-                          widget.subRedditData[1]['data']['children'].length,
+                    SizedBox(
+                      height: 300.0,
+                      child: ListView.builder(
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              Card(
+                                child: ListTile(
+                                  title: Text(
+                                      'Author: ${widget.subRedditData[1]['data']['children'][index]['data']['author']}'),
+                                  subtitle: Text(widget.subRedditData[1]['data']
+                                      ['children'][index]['data']['body']),
+                                ),
+                                color: Colors.white70,
+                              )
+                            ],
+                          );
+                        },
+                        itemCount:
+                            widget.subRedditData[1]['data']['children'].length,
+                      ),
                     ),
-                  ),
-                ],
-              ))
-            ],
+                  ],
+                ))
+              ],
+            ),
           )),
     );
   }
